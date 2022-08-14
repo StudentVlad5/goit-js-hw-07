bff8714b8951b70ea9fe5c525ade9699857b9e6a
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2022-08-14 10:26:04 +0300

    add

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,24 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const ref = {
+    containerForGallery: document.querySelector('.gallery'),
+}
 
-console.log(galleryItems);
+let galleryItem = '';
+const buildGallery = galleryItems.map((key)=>{
+     galleryItem += `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${key.original}">
+                <img
+                    class="gallery__image"
+                    src="${key.preview}"
+                    data-source="${key.original}"
+                    alt="${key.description}"
+                />
+            </a>
+        </div>`;    
+})
+
+ref.containerForGallery.insertAdjacentHTML("beforebegin", galleryItem)
+
+
