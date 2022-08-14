0010207eb8fe1d110b4d0d8156a75d22bd40262f
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2022-08-14 18:01:10 +0300

    main

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,24 +1,43 @@
 import { galleryItems } from './gallery-items.js';
+import * as basicLightbox from '../dist/basicLightbox.min.js'
 // Change code below this line
 const ref = {
     containerForGallery: document.querySelector('.gallery'),
 }
 
+console.log(basicLightbox)
+
 let galleryItem = '';
 const buildGallery = galleryItems.map((key)=>{
      galleryItem += `
         <div class="gallery__item">
-            <a class="gallery__link" href="${key.original}">
+        <a class="gallery__link" href="#">
                 <img
                     class="gallery__image"
                     src="${key.preview}"
                     data-source="${key.original}"
                     alt="${key.description}"
                 />
-            </a>
+                </a>
         </div>`;    
 })
 
-ref.containerForGallery.insertAdjacentHTML("beforebegin", galleryItem)
+ref.containerForGallery.insertAdjacentHTML("afterbegin", galleryItem)
 
+ref.containerForGallery.addEventListener(`click`,openModalWindow)
+
+function openModalWindow (event) {
+    // if(event.target.value !== ".gallery__link"){return}
+    console.log(event.target.dataset.source);
+    event.preventDefault();
+
+    const instance = basicLightbox.create(`
+    <div class="modal">
+    <img src="${event.target.dataset.source}" width="800" height="600">
+        <a>Close</a>
+    </div>
+`)
+
+instance.show()
+}
 
