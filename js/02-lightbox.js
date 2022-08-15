8a7f035749aaa4952dbcb5a1c4e9c32ae80ef4af
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2022-08-15 20:05:04 +0300

    main

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from "../node_modules/simplelightbox/dist/simple-lightbox.esm.js"
 // Change code below this line
 
 console.log(galleryItems);
+
+const ref = {
+    containerForGallery: document.querySelector('.gallery'),
+}
+
+let galleryItem = '';
+const buildGallery = galleryItems.map((key)=>{
+     galleryItem += `
+        <div class="gallery__item">
+        <a class="gallery__link" href="${key.original}">
+                <img
+                    class="gallery__image"
+                    src="${key.preview}"
+                    data-source="${key.original}"
+                    alt="${key.description}"
+                />
+                </a>
+        </div>`;    
+})
+
+ref.containerForGallery.innerHTML = galleryItem;
+
+
+new SimpleLightbox('.gallery__link', { 
+    
+ });
